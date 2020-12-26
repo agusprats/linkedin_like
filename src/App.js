6a3f8ce6b7b6f7c357b4c2657ee0fec5c5411b5f
Agustina Prats <agusprats@gmail.com>
2020-12-26 20:33:00 -0300

    Feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Feed from './Feed';
 import Sidebar from './Sidebar';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <div className="app__body">
         <Sidebar/>
+        <Feed/>
       </div>
     </div>
   );
