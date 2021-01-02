a43441aa5fb8d485f930462c82d9d114fad982e6
Agustina Prats <agusprats@gmail.com>
2021-01-02 13:30:38 -0300

    firebase config

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Feed.css';
 import InputOption from './InputOption'
 import CreateIcon from '@material-ui/icons/Create';
@@ -6,16 +6,44 @@ import ImageIcon from '@material-ui/icons/Image';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from './Post';
+import {db} from './firebase';
+import firebase from 'firebase';
 
 function Feed() {
+    const[input, setInput] = useState('');
+    const[posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection('posts').onSnapshot(snapshot => (
+        setPosts(
+            snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data(),
+            }))
+        )
+    ))
+}, [])
+
+    const sendPost = e => {
+        e.preventDefault();
+
+        db.collection('posts').add({
+            name: 'Agus Dev',
+            description: 'this is a test',
+            message: input,
+            photoUrl: '',
+            timestamp: firebase.firestore.fieldValue.serverTimestamp()
+        })
+    };
     return (
         <div className="feed">
             <div className="feed__inputContainer">
                 <div className="feed__input">
                     <CreateIcon/>
                     <form>
-                        <input type='text' />
-                        <button type='submit'>Send</button>
+                        <input value={input} onChange={e => setInput(e.target.value)} type='text' />
+                        <button onClick={sendPost} type='submit'>Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -26,9 +54,19 @@ function Feed() {
                     <InputOption Icon={EventNoteIcon} title='Event' 
                     color="#70B5F9" />
                     <InputOption Icon={CalendarViewDayIcon} title='Write article'
-                    color="#7fc15e" />
+                    color="#7fc15e"
+                    />
                 </div>
             </div>
+            
+            {/* Posts */}
+            {posts.map(({ id, data: { name, description, message, photoUrl }}) => (
+                <Post/>
+            ))}
+            <Post 
+            name='Agus Prats' 
+            description='this is a test'
+            message='wow this works'/>
         </div>
     )
 }
